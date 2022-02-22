250513a4a7adea3211c3e6d45f650ccf008e8bea
GI10 <tadus@op.pl>
2022-02-22 14:29:09 +0100

    Adding file cut_wood.py

---
+++ cut_wood.py
@@ -0,0 +1,16 @@
+"""Given some pieces of wood. It given by an integer array woods(argument 1), each element is the length of wood. Cut
+them into small pieces to guarantee you could have equal or more than n(argument 2) pieces with the same length(length
+should be integer). Your task is find what is the longest length you can get from these woods? Return the maximum
+length of the small pieces.
+woodCut([232, 124, 456], 7) === 114
+woodCut([232, 124, 456], 20) === 38
+woodCut([232, 124, 456], 1) === 456
+woodCut([232, 124, 456], 2) === 232
+woodCut([232, 124, 456], 3) === 228
+woodCut([1, 1, 1], 4) === 0
+woodCut([1, 1, 1], 3) === 1
+"""
+
+
+def cut_wood(array, n):
+    pass
